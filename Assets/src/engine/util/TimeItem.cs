c7f0b9aa9e86b242fccd487ec2e31a1db74a243f
lits <rs_zs@163.com>
2014-12-26 16:59:57 +0800

    timer

---
+++ Assets/src/engine/util/TimeItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TimeItem : MonoBehaviour
+{
+    public CallBackFun fun = null;
+    public CallBackRemoveFun removeFun = null;
+
+    private static int UID = 0;
+
+    public int id;
+    public int total;
+    public int cur;
+
+
+    public static TimeItem Init(CallBackFun fun, CallBackRemoveFun rf, float intervalTime, int time = int.MaxValue)
+    {
+        TimeItem ins = new TimeItem();
+        ins.id = UID++;
+        ins.fun = fun;
+        ins.removeFun = rf;
+        ins.total = time;
+        ins.cur = 0;
+
+        ins.InvokeRepeating("back", intervalTime, intervalTime);
+        return ins;
+    }
+
+    public void back()
+    {
+        cur++;
+        if (cur >= total)
+        {
+            Cancel();
+            fun();
+            removeFun(this);
+        }
+    }
+
+    public void Cancel()
+    {
+        CancelInvoke("back");
+    }
+}
+
---
+++ Assets/src/engine/util/Timer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public delegate void CallBackFun();
+public delegate void CallBackRemoveFun(TimeItem item);
+public class Timer
+{
+    private static List<TimeItem> _items = null;
+    public static int AddTimer(CallBackFun fun, float intervalTime, int time)
+    {
+        TimeItem item = GetTimer(fun);
+        if (item != null)
+        {
+            RemoveTimer(item);
+        }
+        item = TimeItem.Init(fun, RemoveTimer, intervalTime, time);
+        _items.Add(item);
+        return item.id;
+    }
+
+    public static bool RemoveTimer(int id)
+    {
+        int count = _items.Count;
+        for (int i = 0; i < count; i++ )
+        {
+            TimeItem item = _items[i];
+            if(item.id == id)
+            {
+                _items.RemoveAt(i);
+                item.Cancel();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void RemoveTimer(TimeItem item)
+    {
+        if(_items.Remove(item))
+        {
+            item.Cancel();
+        }
+    }
+
+    public static TimeItem GetTimer(CallBackFun fun)
+    {
+        foreach(TimeItem i in _items)
+        {
+            if(i.fun == fun)
+            {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static TimeItem GetTimer(int id)
+    {
+        foreach (TimeItem i in _items)
+        {
+            if (i.id == id)
+            {
+                return i;
+            }
+        }
+        return null;
+    }
+
+}
+
+
+
